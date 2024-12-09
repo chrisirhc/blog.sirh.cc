99eeba8dacd89cb5028fcbe4a66da2163350c5c7
Chris Chua <chris.sirhc@gmail.com>
2024-12-09 14:57:11 -0500

    Update description

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const SITE_TITLE = "Chris Chua's Blog";
-export const SITE_DESCRIPTION = "My personal blog about web development, software engineering, and other things I find interesting.";
+export const SITE_DESCRIPTION = "My personal blog about software engineering, developer tools, open source, and other things I find interesting.";
